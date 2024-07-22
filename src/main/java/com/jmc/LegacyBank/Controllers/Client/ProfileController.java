9368ab929b3a6898f078743223bd2f11ca458307
patilshreyas22 <shreyaspatil.scoe.comp@gmail.com>
2024-07-22 22:04:57 +0530

    Changes in the Profile of ClientMenu

--- src/main/java/com/jmc/LegacyBank/Controllers/Client/ProfileController.java
+++ src/main/java/com/jmc/LegacyBank/Controllers/Client/ProfileController.java
@@ -2,7 +2,9 @@ package com.jmc.LegacyBank.Controllers.Client;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,6 +12,12 @@ import java.util.ResourceBundle;
 public class ProfileController implements Initializable {
 
 
+    public Label nameValue;
+    public Label passwordValue;
+    public Label lastLoginValue;
+    public PasswordField currentPasswordField;
+    public PasswordField newPasswordField;
+    public Button changePasswordButton;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
